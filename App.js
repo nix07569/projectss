facd029b31bec0bb8f61723a6e2b8d091b9e1e3e
Nandkishore Pareek <164609124+nix07569@users.noreply.github.com>
2026-03-25 02:39:07 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,60 +1,71 @@
- <HBox id="dashboardTopHalf" width="100%" class="sapUiSmallMarginBottom sapUiSmallMarginBeginEnd">
-                            
-                            <VBox id="kpiGrid" width="40%" class="sapUiSmallMarginEnd">
-                                <HBox id="kpiTopRow" width="100%" class="sapUiSmallMarginBottom">
-                                    <VBox id="cardUploaded" class="customKpiCard sapUiSmallMarginEnd" width="50%">
-                                        <Text id="titleUploaded" text="Total MFU Uploaded" class="kpiTitle" />
-                                        <VBox id="valBoxUp"><Text id="valUploaded" text="71" class="kpiValue" /><Text id="subUploaded" text="MFU_COUNT" class="kpiSubText" /></VBox>
-                                    </VBox>
-                                    <VBox id="cardApproved" class="customKpiCard" width="50%">
-                                        <Text id="titleApproved" text="Total MFU Approved" class="kpiTitle" />
-                                        <VBox id="valBoxApp"><Text id="valApproved" text="60" class="kpiValue" /><Text id="subApproved" text="MFU_COUNT" class="kpiSubText" /></VBox>
-                                    </VBox>
-                                </HBox>
-                                <HBox id="kpiBottomRow" width="100%">
-                                    <VBox id="cardOnboarded" class="customKpiCard sapUiSmallMarginEnd" width="50%">
-                                        <Text id="titleOnboarded" text="MFU Template Onboarded" class="kpiTitle" />
-                                        <VBox id="valBoxOnb"><Text id="valOnboarded" text="114" class="kpiValue" /><Text id="subOnboarded" text="MFU_COUNT" class="kpiSubText" /></VBox>
-                                    </VBox>
-                                    <VBox id="cardRejected" class="customKpiCard" width="50%">
-                                        <Text id="titleRejected" text="Total MFU Rejected" class="kpiTitle" />
-                                        <VBox id="valBoxRej"><Text id="valRejected" text="-" class="kpiValue" /><Text id="subRejected" text="MFU_COUNT" class="kpiSubText" /></VBox>
-                                    </VBox>
-                                </HBox>
-                            </VBox>
-                            
-                            <Panel id="mainChartPanel" width="60%" class="customKpiCard sapUiSmallMarginBegin">
-                                <Title id="stackedTitle" text="MFU Templates by Modules" level="H3" class="sapUiSmallMarginBottom"/>
-                                
-                                <viz:VizFrame id="stackedChart" vizType="stacked_column" width="100%" height="320px" 
-                                    uiConfig="{applicationSet:'fiori'}"
-                                    vizProperties="{
-                                        'plotArea': {
-                                            'dataLabel': { 'visible': true, 'showTotal': true },
-                                            'colorPalette': ['#2B4A8E', '#42B8D4', '#3AC582']
-                                        },
-                                        'title': { 'visible': true, 'text': 'Title of Stacked Column Chart' },
-                                        'legendGroup': { 'layout': { 'position': 'top' } }
-                                    }">
-                                    
-                                    <viz:dataset>
-                                        <viz.data:FlattenedDataset id="stackDataset" data="{/StackedData}">
-                                            <viz.data:dimensions>
-                                                <viz.data:DimensionDefinition id="dimModule" name="Module" value="{Module}" />
-                                            </viz.data:dimensions>
-                                            <viz.data:measures>
-                                                <viz.data:MeasureDefinition id="measApproved" name="approved" value="{approved}" />
-                                                <viz.data:MeasureDefinition id="measPending" name="pending" value="{pending}" />
-                                                <viz.data:MeasureDefinition id="measUploaded" name="uploaded" value="{uploaded}" />
-                                            </viz.data:measures>
-                                        </viz.data:FlattenedDataset>
-                                    </viz:dataset>
-                                    
-                                    <viz:feeds>
-                                        <viz.feeds:FeedItem id="feedValueAxis" uid="valueAxis" type="Measure" values="approved,pending,uploaded" />
-                                        <viz.feeds:FeedItem id="feedCategoryAxis" uid="categoryAxis" type="Dimension" values="Module" />
-                                    </viz:feeds>
-                                    
-                                </viz:VizFrame>
-                            </Panel>
-                        </HBox>
+<HBox id="dashboardBottomHalf" width="100%" class="sapUiSmallMarginBottom sapUiSmallMarginBeginEnd flushTopContainer" justifyContent="SpaceBetween">
+    
+    <VBox id="cardPie" width="24%" class="customChartCard">
+        <Text id="pieTitleText" text="MFU Templates by Count" class="kpiTitle sapUiSmallMarginBottom" />
+        <viz:VizFrame id="pieChart" vizType="pie" width="100%" height="220px" uiConfig="{applicationSet:'fiori'}"
+            vizProperties="{ title: { visible: false }, legendGroup: { layout: { position: 'top' } } }">
+            <viz:dataset>
+                <viz.data:FlattenedDataset id="pieDataset" data="{/PieData}">
+                    <viz.data:dimensions><viz.data:DimensionDefinition id="pieDimModule" name="Module" value="{Module}" /></viz.data:dimensions>
+                    <viz.data:measures><viz.data:MeasureDefinition id="pieMeasPerc" name="Percentage" value="{Percentage}" /></viz.data:measures>
+                </viz.data:FlattenedDataset>
+            </viz:dataset>
+            <viz:feeds>
+                <viz.feeds:FeedItem id="pieFeedSize" uid="size" type="Measure" values="Percentage" />
+                <viz.feeds:FeedItem id="pieFeedColor" uid="color" type="Dimension" values="Module" />
+            </viz:feeds>
+        </viz:VizFrame>
+    </VBox>
+
+    <VBox id="cardMonth" width="24%" class="customChartCard">
+        <Text id="titleMonth" text="Top 3 MFU Templates by&#10;Usage&#10;(by Month)" class="kpiTitle sapUiSmallMarginBottom" />
+        <viz:VizFrame id="chartMonth" vizType="column" width="100%" height="220px" uiConfig="{applicationSet:'fiori'}"
+            vizProperties="{ title: { visible: false }, legend: { visible: false }, plotArea: { dataLabel: { visible: true }, colorPalette: ['#42B8D4'] } }">
+            <viz:dataset>
+                <viz.data:FlattenedDataset id="dsMonth" data="{/MonthData}">
+                    <viz.data:dimensions><viz.data:DimensionDefinition id="dimMonth" name="Module" value="{Module}" /></viz.data:dimensions>
+                    <viz.data:measures><viz.data:MeasureDefinition id="measMonth" name="Usage" value="{Usage}" /></viz.data:measures>
+                </viz.data:FlattenedDataset>
+            </viz:dataset>
+            <viz:feeds>
+                <viz.feeds:FeedItem id="feedMonthVal" uid="valueAxis" type="Measure" values="Usage" />
+                <viz.feeds:FeedItem id="feedMonthCat" uid="categoryAxis" type="Dimension" values="Module" />
+            </viz:feeds>
+        </viz:VizFrame>
+    </VBox>
+
+    <VBox id="cardQuarter" width="24%" class="customChartCard">
+        <Text id="titleQuarter" text="Top 3 MFU Templates by&#10;Usage&#10;(by Quarter)" class="kpiTitle sapUiSmallMarginBottom" />
+        <viz:VizFrame id="chartQuarter" vizType="column" width="100%" height="220px" uiConfig="{applicationSet:'fiori'}"
+            vizProperties="{ title: { visible: false }, legend: { visible: false }, plotArea: { dataLabel: { visible: true }, colorPalette: ['#2B4A8E'] } }">
+            <viz:dataset>
+                <viz.data:FlattenedDataset id="dsQuarter" data="{/QuarterData}">
+                    <viz.data:dimensions><viz.data:DimensionDefinition id="dimQuarter" name="Module" value="{Module}" /></viz.data:dimensions>
+                    <viz.data:measures><viz.data:MeasureDefinition id="measQuarter" name="Usage" value="{Usage}" /></viz.data:measures>
+                </viz.data:FlattenedDataset>
+            </viz:dataset>
+            <viz:feeds>
+                <viz.feeds:FeedItem id="feedQuarterVal" uid="valueAxis" type="Measure" values="Usage" />
+                <viz.feeds:FeedItem id="feedQuarterCat" uid="categoryAxis" type="Dimension" values="Module" />
+            </viz:feeds>
+        </viz:VizFrame>
+    </VBox>
+
+    <VBox id="cardYear" width="24%" class="customChartCard">
+        <Text id="titleYear" text="Top 3 MFU Templates by&#10;Usage&#10;(by Year)" class="kpiTitle sapUiSmallMarginBottom" />
+        <viz:VizFrame id="chartYear" vizType="column" width="100%" height="220px" uiConfig="{applicationSet:'fiori'}"
+            vizProperties="{ title: { visible: false }, legend: { visible: false }, plotArea: { dataLabel: { visible: true }, colorPalette: ['#3AC582'] } }">
+            <viz:dataset>
+                <viz.data:FlattenedDataset id="dsYear" data="{/YearData}">
+                    <viz.data:dimensions><viz.data:DimensionDefinition id="dimYear" name="Module" value="{Module}" /></viz.data:dimensions>
+                    <viz.data:measures><viz.data:MeasureDefinition id="measYear" name="Usage" value="{Usage}" /></viz.data:measures>
+                </viz.data:FlattenedDataset>
+            </viz:dataset>
+            <viz:feeds>
+                <viz.feeds:FeedItem id="feedYearVal" uid="valueAxis" type="Measure" values="Usage" />
+                <viz.feeds:FeedItem id="feedYearCat" uid="categoryAxis" type="Dimension" values="Module" />
+            </viz:feeds>
+        </viz:VizFrame>
+    </VBox>
+
+</HBox>
