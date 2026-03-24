9650fdae0340a7b37c03e5fc87b5b41c7369e7d0
Nandkishore Pareek <164609124+nix07569@users.noreply.github.com>
2026-03-24 23:57:30 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,41 @@
+<Panel id="mainChartPanel" width="60%" class="customKpiCard sapUiSmallMarginBegin">
+    <Title id="stackedTitle" text="MFU Templates by Modules" level="H3" class="sapUiSmallMarginBottom"/>
+    
+    <viz:VizFrame id="stackedChart" vizType="stacked_column" width="100%" height="320px" 
+        uiConfig="{applicationSet:'fiori'}"
+        vizProperties="{
+            'plotArea': {
+                'dataLabel': { 'visible': true, 'showTotal': true },
+                'colorPalette': ['#2B4A8E', '#42B8D4', '#3AC582']
+            },
+            'title': { 'visible': false },
+            'legendGroup': { 'layout': { 'position': 'top' } }
+        }">
+        
+        <viz:dataset>
+            <viz.data:FlattenedDataset id="stackDataset" data="{/StackedData}">
+                <viz.data:dimensions>
+                    <viz.data:DimensionDefinition id="dimModule" name="Module" value="{Module}" />
+                </viz.data:dimensions>
+                <viz.data:measures>
+                    <viz.data:MeasureDefinition id="measApproved" name="approved" value="{approved}" />
+                    <viz.data:MeasureDefinition id="measPending" name="pending" value="{pending}" />
+                    <viz.data:MeasureDefinition id="measUploaded" name="uploaded" value="{uploaded}" />
+                </viz.data:measures>
+            </viz.data:FlattenedDataset>
+        </viz:dataset>
+        
+        <viz:feeds>
+            <viz.feeds:FeedItem id="feedValueAxis" uid="valueAxis" type="Measure" values="approved,pending,uploaded" />
+            <viz.feeds:FeedItem id="feedCategoryAxis" uid="categoryAxis" type="Dimension" values="Module" />
+        </viz:feeds>
+        
+    </viz:VizFrame>
+</Panel>
+
+
+
+bdbsbdb
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel" // 1. Import the JSONModel tool
